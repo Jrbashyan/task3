b8b585bba7819da1181484288b535d911b93746f
Jrbashyan <jrbashyan@gmail.com>
2017-05-28 18:12:19 +0400

    carusel

--- js/js.js
+++ js/js.js
@@ -1,4 +1,26 @@
 
 /*Carousel Head*/
 $(document).ready(function(){
+
+	$('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    animateOut: 'fadeOut', 
+    responsive:{
+        0:{
+            items:1
+        },
+        450:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
 });
